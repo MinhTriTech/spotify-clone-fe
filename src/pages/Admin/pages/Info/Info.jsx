44c35c4945a4348ee58fe87c25a0b8c3957bfde3
Nguyễn Quốc Đại <nhochiprow13@gmail.com>
2025-05-03 19:22:33 +0700

    xong UserInfo

--- src/pages/Admin/pages/Info/Info.jsx
+++ src/pages/Admin/pages/Info/Info.jsx
@@ -4,11 +4,16 @@ import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../../../../store/store";
 import { handleLogout } from "../../../../store/slices/auth";
 import { logout } from "../../../../services/auth";
-import { useState } from "react";
+import { useSelector } from "react-redux";
 const InfoUser = () => {
-    const [user, setUser] = useState("");
     const dispatch = useAppDispatch();
     const navigate = useNavigate();
+
+    const user = useSelector((state) => state.auth.user?.user_info);
+    const userId = user?.id;
+    const userEmail = user?.email;
+    const userUsername = user?.username;
+    console.log("USER: ", userId + userEmail + userUsername);
 
     const Logout = async () => {
         try {
@@ -31,27 +36,28 @@ const InfoUser = () => {
                         <span className="text-white text-3xl">T</span>
                     </div>
                     <div className="text-white ml-4">
-                        <p className="text-xl">Dai123@</p>
-                        <p className="text-sm">Người dùng thông thường</p>
+                        <p className="text-xl">{userUsername}</p>
+                        <p className="text-sm">{user?.is_staff ? "Người quản trị" : "Người dùng thông thường"}</p>
                     </div>
                     <div className="ml-auto mr-6">
-                        <ButtonAdmin icon={faPenToSquare} color="spotifyLightBlack" textHover="white" title="Chỉnh sửa hồ sơ" />
+                        {/* <ButtonAdmin icon={faPenToSquare} color="spotifyLightBlack" textHover="white" title="Chỉnh sửa hồ sơ" /> */}
                         <ButtonAdmin icon={faRightFromBracket} color="spotifyGreen" textHover="white" title="Đăng xuất" onClick={Logout} />
                     </div>
                 </div>
                 <div className="text-white w-full px-8 py-8 flex flex-wrap gap-4 h-fit bg-black">
                     <div className="w-full h-fit flex py-4">
                         <div className="w-[50%]">
-                            <InputCombo type="text" placeholder="Nhập tên Họ của bạn" label="Họ" name="first_name" value="haha" />
-                            <InputCombo type="text" placeholder="Nhập username" label="Username" name="username" value="haha" />
-                            <InputCombo type="text" placeholder="Nhập email" label="Email" name="email" value="haha" />
-                            <InputCombo type="text" placeholder="Phân loại" label="Phân loại" name="email" value="Admin" />
+                            {/* <InputCombo type="text" placeholder="Nhập tên Họ của bạn" label="Họ" name="first_name" value="haha" /> */}
+                            <InputCombo type="text" placeholder="Nhập username" label="Username" name="username" value={userUsername || ""} />
+                            {/* <InputCombo type="text" placeholder="Phân loại" label="Phân loại" name="email" value="Admin" /> */}
                         </div>
                         <div className="w-[50%]">
-                            <InputCombo type="text" placeholder="Nhập Tên của bạn" label="Tên" name="last_name" value="ádkjaskdlasd" />
+                            <InputCombo type="text" placeholder="Nhập email" label="Email" name="email" value={userEmail || ""} />
+
+                            {/* <InputCombo type="text" placeholder="Nhập Tên của bạn" label="Tên" name="last_name" value="ádkjaskdlasd" />
                             <InputCombo type="password" placeholder="Nhập mật khẩu" label="Mật khẩu" name="password" value="012344562412" />
                             <InputCombo type="text" placeholder="Nhập mật khẩu" label="Trạng thái" name="password" value="Đang hoạt động" />
-                            <InputCombo type="text" placeholder="Nhập mật khẩu" label="Superuser" name="password" value="Phải" />
+                            <InputCombo type="text" placeholder="Nhập mật khẩu" label="Superuser" name="password" value="Phải" /> */}
                         </div>
                     </div>
                 </div>
