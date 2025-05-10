9058be89f71c7a6d8ba75957b9b2e96d056c2986
Ngô Hoàng Minh Trí <hoangminhtri.ngo@gmail.com>
2025-05-10 22:45:36 +0700

    10/5 22h45 sửa playlist

--- src/services/playlists.js
+++ src/services/playlists.js
@@ -52,6 +52,23 @@ const removePlaylistItems = async (playlist_id, song_id) => {
   });
 };
 
+const changePlaylistDetails = async (playlist_id, data) => {
+  const formData = new FormData();
+
+  for (const key in data) {
+    if (data[key] !== undefined && data[key] !== null) {
+      formData.append(key, data[key]);
+    }
+  }
+
+  return await axios.put(`/api/music/playlists/${playlist_id}/update/`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+};
+
+
 export const playlistService = {
   fetchTopTracks,
   fecthPlaylists,
@@ -65,4 +82,5 @@ export const playlistService = {
   getPlaylist,
   addPlaylistItems,
   removePlaylistItems,
+  changePlaylistDetails
 };
